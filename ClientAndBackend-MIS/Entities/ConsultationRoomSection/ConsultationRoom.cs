db5d72c8330ed67adb6a158c9c2f7e6524c276ad
José Daniel Flores Morales <dflores@ravisa.com>
2023-01-08 21:02:01 -0600

    Added remaining properties to ConsultationRoom Added new Entity "Drug".

--- ClientAndBackend-MIS/Entities/ConsultationRoomSection/ConsultationRoom.cs
+++ ClientAndBackend-MIS/Entities/ConsultationRoomSection/ConsultationRoom.cs
@@ -1,7 +1,16 @@
-﻿namespace ClientAndBackend_MIS.Entities.ConsultationRoomSection
+﻿using ClientAndBackend_MIS.Entities.DoctorSection;
+using System.Collections.Generic;
+using ClientAndBackend_MIS.Entities.PatientSection;
+using ClientAndBackend_MIS.Entities.DrugSection;
+
+namespace ClientAndBackend_MIS.Entities.ConsultationRoomSection
 {
     public class ConsultationRoom
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
+        public ICollection<Doctor> Doctors { get; set; }
+        public ICollection<Patient> PatientsForRoutineChecks { get; set; }
+        public ICollection<Drug> AvailableDrugs { get; set; }
+
     }
 }
---
+++ ClientAndBackend-MIS/Entities/DrugSection/Drug.cs
@@ -0,0 +1,17 @@
+﻿using ClientAndBackend_MIS.Entities.Shared;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientAndBackend_MIS.Entities.DrugSection
+{
+    public class Drug
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        [Required]
+        public Money AverageCost { get; set; }
+    }
+}
